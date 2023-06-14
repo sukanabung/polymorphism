bc524694d9d9552298873431a19355f98541c9b3
Muhammad Yudha Maputra <yudhay@students.amikom.ac.id>
2023-06-14 14:27:50 +0700

    first commit

---
+++ prak9/PrinterWindows.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+
+        }
+        public virtual void Print()
+        {
+
+        }
+    }
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nEpson display dimension : 10*11");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+    }
+    public class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nCanon display dimension : 9.5*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing....");
+        }
+    }
+    public class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nLaserJet display dimension : 12*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing....");
+        }
+    }
+    public class Salah : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("\nDisplay dimension : not detected");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Printer not detected");
+        }
+    }
+}
